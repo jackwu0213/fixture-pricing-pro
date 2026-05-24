9c5b3be177cbf5ab34edf31d9360b19cf69d697a
Jack Wu <twu0213@gmail.com>
2026-05-23 22:47:44 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/fixture-pricing-pro/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/estimate" element={<EstimateWorkflow />} />
